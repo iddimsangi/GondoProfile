ca29098e301f146780512f57f8f4572bdf939b6d
iddimsangi <adammsangi@gmail.com>
2025-01-13 01:05:59 +0300

    Fix build output directory to dist

--- gulpfile.js
+++ gulpfile.js
@@ -3,50 +3,41 @@ const fileinclude = require("gulp-file-include");
 const sass = require("gulp-sass")(require("node-sass"));
 const sassGlob = require("gulp-sass-glob");
 const rename = require("gulp-rename");
-const through2 = require("through2");
-const connect = require("gulp-connect");
 const fs = require("fs");
 
-// Backup `index.html` before build
-gulp.task("backup-index", function (done) {
-  if (fs.existsSync("./index.html")) {
-    fs.copyFileSync("./index.html", "./index.backup.html");
-  }
-  done();
-});
+// Paths
+const paths = {
+  html: {
+    src: ["./src/*.html", "./src/**/*.html"],
+    dest: "./dist/",
+  },
+  sass: {
+    src: ["./scss/*.scss", "./scss/**/*.scss", "!**/_*/**"],
+    dest: "./dist/css/",
+  },
+};
 
-// Restore `index.html` after build
-gulp.task("restore-index", function (done) {
-  if (fs.existsSync("./index.backup.html")) {
-    fs.renameSync("./index.backup.html", "./index.html");
-  }
-  done();
-});
-
-// File include task (excluding index.html)
+// File include task
 gulp.task("fileinclude", function () {
   return gulp
-    .src(["./src/*.html", "./src/**/*.html", "!./src/index.html"])
+    .src(paths.html.src)
     .pipe(
       fileinclude({
         prefix: "@@",
         basepath: "@file",
       })
     )
-    .pipe(gulp.dest("./"));
+    .pipe(gulp.dest(paths.html.dest));
 });
 
 // Sass compilation
 gulp.task("sass", function () {
   return gulp
-    .src(["./scss/*.scss", "./scss/**/*.scss", "!**/_*/**"])
+    .src(paths.sass.src)
     .pipe(sassGlob())
     .pipe(sass())
-    .pipe(gulp.dest("./css"));
+    .pipe(gulp.dest(paths.sass.dest));
 });
 
 // Default task
-gulp.task(
-  "default",
-  gulp.series("backup-index", "fileinclude", "sass", "restore-index")
-);
+gulp.task("default", gulp.series("fileinclude", "sass"));
